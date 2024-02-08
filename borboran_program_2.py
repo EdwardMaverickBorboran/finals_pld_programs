361ff1b1aa577acdb2b9079bfb27389eec501c2d
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-02-09 02:37:51 +0800

    Again, I added some comments first to indicate on what my code was before I try to rework on it.

---
+++ borboran_program_2.py
@@ -0,0 +1,11 @@
+# Program 1
+
+# Write a program where the user can their name and then it will count how many vowels are there, Big and small.
+# Ex. Danilo -> 3
+
+# The code that I created:
+
+# name_input = input("Enter your name:")
+# vowels = ["a", "e", "i", "o", "u"]
+# count_vowels = vowels.count()
+# print("count_vowels")
